3be5f0be57d70767b06545cb19e581a886d751af
Dollarkillerx <dollarkiller@vip.qq.com>
2019-09-06 14:12:29 +0800

    add

--- web/controller/download.go
+++ web/controller/download.go
@@ -12,20 +12,20 @@ import (
 	"github.com/dollarkillerx/easyutils/clog"
 	"github.com/kataras/iris"
 	"log"
+	"runtime"
 	"strconv"
 	"strings"
+	"sync"
 	"time"
 )
 
 var (
 	num      int
-	dataChan chan *defs.YouTuBeRsq
-	dataMax  chan int
+	lock     sync.Mutex
 )
 
 func init() {
-	dataChan = make(chan *defs.YouTuBeRsq, config.MyConfig.App.TaskNum)
-	dataMax = make(chan int, config.MyConfig.App.TaskNum)
+
 	log.Print("队列初始化成功！")
 	num = 0
 }
@@ -47,8 +47,10 @@ func Download(ctx iris.Context) {
 		return
 	}
 
-	// 查询数据是否存在如果存在则跳过
+	//查询数据是否存在如果存在则跳过
+	lock.Lock()
 	exit := GetDataExit(input.Id)
+	lock.Unlock()
 	if !exit {
 		// 如果存在就返回任务以存在
 		resp.Resp(ctx, defs.Task{Code: 400, Msg: "任务以存在"})
@@ -58,18 +60,18 @@ func Download(ctx iris.Context) {
 	// 如果存在就加入下载队列
 
 	// 判断队伍是否满载
-	fmt.Println(len(dataMax))
+	fmt.Println(len(config.DataMax))
 	fmt.Println(config.MyConfig.App.TaskNum)
-	if len(dataMax) >= config.MyConfig.App.TaskNum {
+	if len(config.DataMax) >= config.MyConfig.App.TaskNum {
 		// 队伍满载 返回繁忙信息
 		resp.Resp(ctx, defs.TaskError)
 		fmt.Println("进入满载环节")
 		return
 	} else {
-		dataChan <- &input
-		dataMax <- 1
-
 		resp.Resp(ctx, defs.TaskOk)
+		config.DataChan <- &input
+		config.DataMax <- 1
+
 	}
 
 }
@@ -86,10 +88,13 @@ func GetData(ctx iris.Context) {
 }
 
 func DowTask() {
+	count := 1
 	for {
 		select {
-		case data := <-dataChan:
+		case data, _ := <-config.DataChan:
+			log.Printf("=====================================> 开启%v", len(config.DataMax))
 			// 开启下载协程
+			count = count + 1
 			go dow(data)
 		}
 	}
@@ -98,7 +103,9 @@ func DowTask() {
 // 下载阶段
 func dow(data *defs.YouTuBeRsq) {
 	defer func() {
-		<-dataMax
+		log.Println("==============================================================")
+		fmt.Printf("打印一下释放操作：%v", <-config.DataMax)
+		log.Println("==============================================================")
 	}()
 	// 获取当前时间  生成目录地址
 	s, e := easyutils.TimeGetTimeToString(easyutils.TimeGetNowTimeStr())
@@ -115,6 +122,12 @@ func dow(data *defs.YouTuBeRsq) {
 		panic(e.Error())
 	}
 
+	log.Print(runtime.NumGoroutine())
+
+	time.Sleep(time.Second * 5)
+
+	return
+
 	// 下载文件
 
 	// 失败尝试 10 次 每次 休息 10s
@@ -122,6 +135,7 @@ func dow(data *defs.YouTuBeRsq) {
 	k := 0
 	for {
 		e := video_dow.YoutubeDow(data.Url, path+name+".mp4", "127.0.0.1:8001")
+		panic("==============================================> 错误")
 		if e != nil {
 			k += 1
 			if k < 10 {
